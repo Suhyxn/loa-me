9377a24b96bc855df45d7a706dd31a113f4786b4
Suhyxn <momentomori21489@gmail.com>
2023-10-12 20:10:26 +0900

    style: Global style settings

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { QueryClient, QueryClientProvider } from "react-query";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import "./index.css";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  body {
+    font-size: 1rem;
+    font-family: 'Noto Sans KR', 'Roboto', sans-serif;
+    background-color: #4A4A48;
+  }
+
+  a {
+    color: #000;
+    text-decoration: none;
+  }
+
+  button { 
+    boder: none;
+    cursor: pointer;
+    :focus {
+      outline: none;
+    }
+  }
+
+  input {
+    background-color: transparent;
+    border: none;
+    font-size: 1rem;
+    :focus {
+      outline: none;
+    }
+  }
+`;
 
 const queryClient = new QueryClient();
 
@@ -12,12 +45,8 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
+      <GlobalStyle />
       <App />
     </QueryClientProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
