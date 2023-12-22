3e8c1de028f0efadb9f4ec77dad44916082c56e3
Suhyxn <momentomori21489@gmail.com>
2023-12-22 20:24:44 +0900

    feat: Added function to go to DetailPage.tsx  when pressing enter

--- src/components/SearchArea.tsx
+++ src/components/SearchArea.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
 
 const SearchBox = styled.input`
   width: 100%;
   height: 100%;
+  color: #ffffff;
   outline: 4px solid #ebebe3;
   border-radius: 1rem;
   line-height: 100%;
@@ -24,9 +26,26 @@ const SearchBox = styled.input`
 `;
 
 function SearchArea() {
+  const navigate = useNavigate();
+  const [searchText, setSearchText] = useState("");
+
+  const ChangeSearchText = (e: any) => {
+    setSearchText(e.target.value);
+  };
+
+  const ChangeCharacterPage = (e: any) => {
+    if (e.key === "Enter") {
+      navigate(`/${searchText}`);
+    }
+  };
+
   return (
     <>
-      <SearchBox placeholder="검색할 캐릭터명을 입력해 주세요..." />
+      <SearchBox
+        placeholder="검색할 캐릭터명을 입력해 주세요..."
+        onChange={ChangeSearchText}
+        onKeyDown={ChangeCharacterPage}
+      />
     </>
   );
 }
